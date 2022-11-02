6d4ea68139ed2c0d5f46471515b8559366d3f8b6
dnavmat326 <diegoalmeriense@gmail.com>
2022-11-02 14:01:44 +0100

    update 2 files and delete 1 file

--- ahorcado/controlador.php
+++ ahorcado/controlador.php
@@ -1,13 +1,11 @@
 <?php session_start(); ?>
-<?php include('lib.php'); ?>
 <?php
 
-$diccionario = ["hola"];
+$diccionario = ["hola", "adios", "gato", "perro", "azul", "amarillo", "verde", "negro", "blanco", "arbol", "coche", "ventana", "puerta", "silla", "plato", "mesa", "botella", "gorra", "libro", "lapiz"];
 if ($_POST) {
-
     if (isset($_POST['empezar'])) {
 
-        $_SESSION['palabra'] = str_split($diccionario[0]);
+        $_SESSION['palabra'] = str_split($diccionario[rand(0, count($diccionario) - 1)]);
         $_SESSION['palabraOculta'] = array();
         for ($i = 0; $i < count($_SESSION['palabra']); $i++) {
             array_push($_SESSION['palabraOculta'], "_ ");
@@ -19,13 +17,18 @@ if ($_POST) {
     }
 
     if (isset($_POST['letra'])) {
+        if (isset($_SESSION['letras'])) {
+            array_push($_SESSION['letras'], strtolower($_POST['letra']));
+        } else {
+            $_SESSION['letras'] = [strtolower($_POST['letra'])];
+        }
 
         for ($i = 0; $i < count($_SESSION['palabraOculta']); $i++) {
-            if ($_SESSION['palabra'][$i] == $_POST['letra']) {
-                $_SESSION['palabraOculta'][$i] = $_POST['letra'];
+            if ($_SESSION['palabra'][$i] == strtolower($_POST['letra'])) {
+                $_SESSION['palabraOculta'][$i] = strtolower($_POST['letra']);
             }
         }
-        if (!in_array($_POST['letra'], $_SESSION['palabra'])) {
+        if (!in_array(strtolower($_POST['letra']), $_SESSION['palabra'])) {
             $_SESSION['errores']++;
         }
 
@@ -33,8 +36,13 @@ if ($_POST) {
 
         header("Location: index.php?accion=probar");
     }
-    if ($_SESSION['errores'] == 6) {
+
+    if (isset($_POST['volver'])) {
+
         session_destroy();
+
+
+
         header("Location: index.php");
     }
 }
--- ahorcado/index.php
+++ ahorcado/index.php
@@ -1,5 +1,4 @@
 <?php session_start(); ?>
-<?php include('lib.php'); ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -11,52 +10,82 @@
     <title>Ahorcado</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 
+
 </head>
 
 <body>
 
-    <div class='container'>
+    <div class='container-fluid h2'>
 
-        <h2>Ahorcado PHP</h2>
+        <h1 class="text-primary text-center">Ahorcado PHP</h1>
 
+        <div class="container border border-dark border-3">
 
+            <?php
+            if (!isset($_GET['accion'])) {
+            ?>
 
-        <?php
-        if (!isset($_GET['accion'])) {
-        ?>
+                <div class='row'>
 
-            <div class='row'>
+                    <div class='col-3'>
+                        <form action="controlador.php" method='post'>
+                            <button type="submit" name='empezar' class="btn btn-info">Empezar</button>
 
-                <div class='col-3'>
-                    <form action="controlador.php" method='post'>
-                        <button type="submit" name='empezar' class="btn btn-info">Empezar</button>
+                        </form>
+                    </div>
 
-                    </form>
                 </div>
 
-            </div>
+                <?php
+            } else if (isset($_GET['accion'])) {
+                if ($_SESSION['errores'] != 6 && implode($_SESSION['palabraOculta']) != implode($_SESSION['palabra'])) {
+                    echo (implode($_SESSION['palabraOculta']) . "<br>");
+                ?>
+                    <div class='row'>
 
-        <?php
-        } else {
-            echo (implode($_SESSION['palabraOculta']) . "<br>") ?>
-            <div class='row'>
+                        <div class='col-3'>
+                            <form class="form" action="controlador.php" method='post'>
+                                <label for="letra">Letra:&nbsp; </label><input type="text" name="letra" id="letra" size="1" maxlength="1" required>
+                                <button type="submit" name='probar' class="btn btn-info">Probar</button>
 
-                <div class='col-3'>
-                    <form action="controlador.php" method='post'>
-                        <input type="text" name="letra" id="letra" value="letra">
-                        <button type="submit" name='probar' class="btn btn-info">Probar</button>
-
-                    </form>
-                </div>
-
-
-            </div>
-        <?php
-            echo ("Errores: " . $_SESSION['errores'] . "/6" . "<br>");
-        }
-        ?>
+                            </form>
+                        </div>
 
 
+                    </div>
+                    <?php
+                    if (isset($_SESSION['letras'])) {
+                        echo ("Letras usadas: " . implode(" , ", $_SESSION['letras'])) . "<br>";
+                    }
+                }
+                if (implode($_SESSION['palabraOculta']) != implode($_SESSION['palabra'])) {
+                    echo ("<img src='./" . $_SESSION['errores'] . ".jpeg'>" . "<br>");
+                }
+                if ($_SESSION['errores'] == 6 || implode($_SESSION['palabraOculta']) == implode($_SESSION['palabra'])) {
+                    echo ("Solucion: " . implode($_SESSION['palabra']) . "<br><br>");
+                    ?>
+                    <div class='row'>
+
+                        <div class='col-3'>
+                            <form action="controlador.php" method='post'>
+                                <button type="submit" name='volver' class="btn btn-info">Volver a jugar</button>
+
+                            </form>
+                        </div>
+
+
+                    </div>
+            <?php
+                }
+                if (implode($_SESSION['palabraOculta']) == implode($_SESSION['palabra'])) {
+                    echo ("<img src='./7.jpeg'>" . "<br>");
+                }
+            }
+
+            ?>
+
+
+        </div>
     </div>
 
 </body>
