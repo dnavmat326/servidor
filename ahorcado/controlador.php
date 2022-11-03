7cc5d397134d79bd9605417977ae3db31927e8da
dnavmat326 <diegoalmeriense@gmail.com>
2022-11-03 11:39:56 +0100

    cambios

--- ahorcado/controlador.php
+++ ahorcado/controlador.php
@@ -1,11 +1,26 @@
 <?php session_start(); ?>
 <?php
 
-$diccionario = ["hola", "adios", "gato", "perro", "azul", "amarillo", "verde", "negro", "blanco", "arbol", "coche", "ventana", "puerta", "silla", "plato", "mesa", "botella", "gorra", "libro", "lapiz"];
+//Funciones
+
+// Palabra al azar
+function azar($diccionario)
+{
+    return str_split($diccionario[rand(0, count($diccionario) - 1)]);
+}
+
+//Pintar ahorcado
+function pintarAhorcado($numImg)
+{
+    return ("<img class='img-thumbnail' src='./" . $numImg . ".jpeg'>" . "<br>");
+}
+
+$diccionario = ["hola", "adios", "gato", "perro", "azul"/*, "amarillo", "verde", "negro", "blanco", "arbol", "coche", "ventana", "puerta", "silla", "plato", "mesa", "botella", "gorra", "libro", "lapiz"*/];
+// Selecciona palabra del diccionario al azar
 if ($_POST) {
     if (isset($_POST['empezar'])) {
 
-        $_SESSION['palabra'] = str_split($diccionario[rand(0, count($diccionario) - 1)]);
+        $_SESSION['palabra'] = azar($diccionario);
         $_SESSION['palabraOculta'] = array();
         for ($i = 0; $i < count($_SESSION['palabra']); $i++) {
             array_push($_SESSION['palabraOculta'], "_ ");
@@ -15,19 +30,22 @@ if ($_POST) {
 
         header("Location: index.php?accion=inicio");
     }
-
+    // Meter las letras usadas en la sesion
     if (isset($_POST['letra'])) {
         if (isset($_SESSION['letras'])) {
-            array_push($_SESSION['letras'], strtolower($_POST['letra']));
+            if (!in_array(strtolower($_POST['letra']), $_SESSION['letras'])) {
+                array_push($_SESSION['letras'], strtolower($_POST['letra']));
+            }
         } else {
             $_SESSION['letras'] = [strtolower($_POST['letra'])];
         }
-
+        // Comprobar letras introducidas por teclado
         for ($i = 0; $i < count($_SESSION['palabraOculta']); $i++) {
             if ($_SESSION['palabra'][$i] == strtolower($_POST['letra'])) {
                 $_SESSION['palabraOculta'][$i] = strtolower($_POST['letra']);
             }
         }
+        // Contar errores
         if (!in_array(strtolower($_POST['letra']), $_SESSION['palabra'])) {
             $_SESSION['errores']++;
         }
@@ -37,6 +55,7 @@ if ($_POST) {
         header("Location: index.php?accion=probar");
     }
 
+    // Volver a jugar
     if (isset($_POST['volver'])) {
 
         session_destroy();
--- ahorcado/index.php
+++ ahorcado/index.php
@@ -1,5 +1,4 @@
-<?php session_start(); ?>
-
+<?php include('controlador.php'); ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,25 +8,42 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ahorcado</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <style>
+        .center {
+            margin-left: 35%;
+            padding: 3%;
+        }
 
+        .fondo {
+            background-color: #ebebff;
+        }
+
+        .btn-info {
+            background-color: #b8b8ff;
+            border: 2px solid grey;
+            color: #ebebff;
+            font-size: large;
+        }
+    </style>
 
 </head>
 
 <body>
 
-    <div class='container-fluid h2'>
+    <div class='container h2'>
 
-        <h1 class="text-primary text-center">Ahorcado PHP</h1>
 
-        <div class="container border border-dark border-3">
-
+        <div class="container border border-grey border-3 text-center fondo rounded p-3 m-1">
             <?php
+            // Inicio del juego
             if (!isset($_GET['accion'])) {
             ?>
 
                 <div class='row'>
 
-                    <div class='col-3'>
+                    <div class='col-3 center'>
+                        <h1 class="h1 text-center text-primary">Ahorcado PHP</h1>
+
                         <form action="controlador.php" method='post'>
                             <button type="submit" name='empezar' class="btn btn-info">Empezar</button>
 
@@ -37,13 +53,14 @@
                 </div>
 
                 <?php
+                // Pantalla para probar letra
             } else if (isset($_GET['accion'])) {
                 if ($_SESSION['errores'] != 6 && implode($_SESSION['palabraOculta']) != implode($_SESSION['palabra'])) {
                     echo (implode($_SESSION['palabraOculta']) . "<br>");
                 ?>
                     <div class='row'>
 
-                        <div class='col-3'>
+                        <div class='col-3 center'>
                             <form class="form" action="controlador.php" method='post'>
                                 <label for="letra">Letra:&nbsp; </label><input type="text" name="letra" id="letra" size="1" maxlength="1" required>
                                 <button type="submit" name='probar' class="btn btn-info">Probar</button>
@@ -54,19 +71,23 @@
 
                     </div>
                     <?php
+                    // Pintar letras usadas
                     if (isset($_SESSION['letras'])) {
                         echo ("Letras usadas: " . implode(" , ", $_SESSION['letras'])) . "<br>";
                     }
                 }
+                // Pintar ahorcado
                 if (implode($_SESSION['palabraOculta']) != implode($_SESSION['palabra'])) {
-                    echo ("<img src='./" . $_SESSION['errores'] . ".jpeg'>" . "<br>");
+                    echo (pintarAhorcado($_SESSION['errores']));
                 }
+                // Mostrar solucion
                 if ($_SESSION['errores'] == 6 || implode($_SESSION['palabraOculta']) == implode($_SESSION['palabra'])) {
                     echo ("Solucion: " . implode($_SESSION['palabra']) . "<br><br>");
+                    // Pantalla para volver a jugar
                     ?>
                     <div class='row'>
 
-                        <div class='col-3'>
+                        <div class='col-3 center'>
                             <form action="controlador.php" method='post'>
                                 <button type="submit" name='volver' class="btn btn-info">Volver a jugar</button>
 
@@ -78,7 +99,7 @@
             <?php
                 }
                 if (implode($_SESSION['palabraOculta']) == implode($_SESSION['palabra'])) {
-                    echo ("<img src='./7.jpeg'>" . "<br>");
+                    echo (pintarAhorcado("7"));
                 }
             }
 
