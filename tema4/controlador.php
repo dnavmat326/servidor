fe2dfdca8622dfdff9e5fc1bf1668cb339fc9991
dnavmat326 <diegoalmeriense@gmail.com>
2022-11-04 20:43:51 +0100

    cambios

--- tema4/controlador.php
+++ tema4/controlador.php
@@ -161,13 +161,15 @@ if ($_GET['accion'] == "imprimir") {
 
 
     try {
+        $t = 'lihdzvxxuunsofqf';
+        $c = 'ejemploclasephp@gmail.com';
         //Server settings
         $mail->SMTPDebug = SMTP::DEBUG_OFF;                      //Enable verbose debug output
         $mail->isSMTP();                                            //Send using SMTP
         $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'ejemploclasephp@gmail.com';                     //SMTP username
-        $mail->Password   = 'lihdzvxxuunsofqf';                               //SMTP password
+        $mail->Username   = $c;                     //SMTP username
+        $mail->Password   = $t;                               //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
         $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
